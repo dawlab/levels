44c3a396a20b60d0d4c6970745b464faaefab5fc
Dawid <dawidlabno@outlook.com>
2023-06-03 15:57:47 +0200

    Initial Commit

---
+++ Goals/GoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsApp.swift
+//  Goals
+//
+//  Created by Dawid Łabno on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
